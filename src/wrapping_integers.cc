ce507ab2ab1ed108e3dbab42465f103959ae907d
zl90 <mb541@protonmail.com>
2024-10-24 22:10:17 +1100

    Partially implement unwrap method

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -10,8 +10,12 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint32_t n = this->raw_value_;
+  uint32_t isn = zero_point.raw_value_;
+
+  if ( checkpoint < Wrap32::largest_32_bit_integer ) {
+    return n - isn;
+  }
+
+  return ( Wrap32::largest_32_bit_integer ) * ( checkpoint / ( Wrap32::largest_32_bit_integer ) ) + isn - n;
 }
